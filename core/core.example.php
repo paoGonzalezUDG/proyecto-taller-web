7687e539c6df52fabd340f688c5b1178953a79aa
paoGonzalezUDG <paola.n.a.0914@gmail.com>
2026-03-10 16:26:06 -0600

    Arquitectura base del proyecto: MVC, Webpack y Rutas

---
+++ core/core.example.php
@@ -0,0 +1,58 @@
+<?php
+	define('TEMPLATES_HEAD','templates/head.php');
+	define('TEMPLATES_HEADER','templates/header.php');
+	define('TEMPLATES_HEADER_DASHBOARD','templates/header-dashboard.php');
+	define('TEMPLATES_FOOTER','templates/footer.php');
+	define('TEMPLATES_FOOTER_DASHBOARD','templates/footer-dashboard.php');
+	define('TEMPLATES_SOCIAL','templates/social.php');
+	define('TEMPLATES_LOGIN','templates/modal-login.php');
+	define('TEMPLATES_PRELOADER','templates/preloader.php');
+	define('TEMPLATES_API_FACEBOOK','templates/api-facebook.php');
+	define('JS_APP','js/app.js');
+	define('RUTA_CONTROLLER_DIR_DEFAULT','Controller.php');
+	define('RUTA_CONTROLLER_DIR','core/controllers');
+	define('RUTA_ERROR_DIR','core/controllers/error404Controller.php');
+	define('RUTA_DEFAULT_DIR','core/controllers/inicioController.php');
+	define('WEBSITE','NOMBE_DE_TU_SITIO_WEB');
+	define('COPYRIGHT','Copyright &copy; '.date('Y',time()));
+	define('ANIO_ACTUAL', date('Y'));
+	define('CORREO_CONTACTO','TU_CORREO');
+	define('URL','http://proyecto-taller-web.test/');
+	define('URL_FRONT','http://proyecto-taller-web.test/');
+	define('URL_SIN_DIAGONAL','http://proyecto-taller-web.test');
+	define('DOMINIO','DOMINIO');
+	define('HOSTING_PHPMAILER','gmail.com');
+	/*ZONA DE HORARIO*/
+	define('TIMEZONE_FRONT', 'America/Mexico_City');
+	/*CAPTCHA*/
+	define('SITE_KEY','');
+	define('SECRET_KEY','');
+	/*GOOGLE SHEET*/
+	define('GOOGLE_SHEET1','');
+	/* NITROPACK */
+	define("NITROPACK_HOME_URL", "http://proyecto-taller-web.test");
+	define("NITROPACK_SITE_ID", "");
+	define("NITROPACK_SITE_SECRET", "");
+	/*TWITTER*/
+	define('USERNAME_TWITTER','@RED_SOCIAL');
+	define('URL_TWITTER','');
+	/*FACEBOOK*/
+	define('APP_ID_FACEBOOK','');
+	define('URL_FACEBOOK','');
+	/*INSTAGRAM*/
+	define('URL_INSTAGRAM','');
+	/*TIKTOK*/
+	define('URL_TIKTOK','');
+	/*LINKEDIN*/
+	define('URL_LINKEDIN','');
+	/*PINTERES*/
+	define('URL_PINTERES','');
+	/*VIMEO*/
+	define('URL_VIMEO','');
+	/*YOUTUBE*/
+	define('URL_YOUTUBE','');
+	/*WHATSAPP*/
+	define('URL_WHATSAPP','https://wa.me/52NUMERO?text=Hola%20!%20les%20contacto%20desde%20su%20p%C3%A1gina%20web,%20me%20interesa%20m%C3%A1s%20informaci%C3%B3n%20sobre%20sus%20servicios');
+	define('URL_SHARE_WHATSAPP','whatsapp://send?text=Mi página: '.URL_SIN_DIAGONAL);
+	/*GOOGLE MAPS*/
+	define('URL_GOOGLE_MAPS','');
